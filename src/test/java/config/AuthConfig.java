fad69c1f385bab764170f840be8a4416b374a562
ucsus <ucsusman@gmail.com>
2023-01-06 21:09:13 +0300

    Another fix

--- src/test/java/config/AuthConfig.java
+++ src/test/java/config/AuthConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "file:/tmp/auth.properties",
+        "file:/build/resources/test/auth.properties",
         "classpath:auth.properties"
 })
 public interface AuthConfig extends Config {
